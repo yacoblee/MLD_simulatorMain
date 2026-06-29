bc982ba209112e8b989cdddd68c08c8e3e5549c9
yacoblee <68984086+yacoblee@users.noreply.github.com>
2026-06-29 17:51:58 +0900

    Create TcpCommConfig.cs

---
+++ sendProject/sendProject/comm/TcpCommConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sendProject.comm
+{
+    public class TcpCommConfig
+    {
+
+        private TcpCommConfig() { 
+            
+        
+        }
+
+
+
+    }
+}
